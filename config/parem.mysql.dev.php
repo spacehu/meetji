60031d381975bd518965ff3f99841c86d705173f
spacehu <spacehu@tigerhuclub.com>
2018-12-28 22:14:44 -0800

    update

---
+++ config/parem.mysql.dev.php
@@ -0,0 +1,12 @@
+<?php
+
+$db = [
+    'table_pre' => 'mrhu_', //
+    'dbms' => 'mysql', //数据库类型 
+    'host' => '212.64.66.47', //数据库主机名
+    'dbName' => 'meetji_test', //使用的数据库
+    'user' => 'root', //数据库连接用户名
+    'password' => 'JianbiA78&*', //对应的密码
+    'port' => '3306',
+];
+return $db;
