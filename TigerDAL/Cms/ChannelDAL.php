4dc2573e4ee356d3bb8fcae07b9c8fb1051e6abc
spacehu <spacehu@tigerhuclub.com>
2022-03-20 21:38:47 +0800

    fix uid

--- TigerDAL/Cms/ChannelDAL.php
+++ TigerDAL/Cms/ChannelDAL.php
@@ -89,8 +89,7 @@ class ChannelDAL {
         $uid=$_SESSION[\mod\init::$config['shop_name']]['id'];
         $user=UserDAL::getOne($uid);
         if($user['role_id']>1){
-            $role_id=$user['role_id'];
-            return " and add_by = ".$role_id ." ";
+            return " and add_by = ".$uid ." ";
         }
         return "";
     }
